df350ebdee2f947735a45d1e4d05cf7e0ec7793c
MakYamag <ultrasplash@gmail.com>
2022-07-23 12:50:13 +0900

    7/20 Updated

--- nb/nb001_220709.py
+++ nb/nb001_220709.py
@@ -5,16 +5,17 @@
 # - *Name*を用いて同じ家族を同定し、生存率を計算した新たな列*Family_SurvRate*を作成する。
 # - *Ticket*を用いて上6桁が同じチケット番号のグループ分けし、生存率を計算した新たな列*Ticket_SurvRate*を作成。
 
-# In[3]:
+# In[1]:
 
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import string
+from sklearn.utils import shuffle
 
 
-# In[49]:
+# In[2]:
 
 
 #df_train = pd.read_csv('/content/drive/My Drive/Colab Notebooks/data/train.csv')   # Google Colabの場合はこちら
@@ -22,7 +23,7 @@ data_train = pd.read_csv('C:/Users/ultra/Documents/GitHub/data/220417_kaggle_Pra
 data_train.head()
 
 
-# In[50]:
+# In[3]:
 
 
 #df_train = pd.read_csv('/content/drive/My Drive/Colab Notebooks/data/test.csv')   # Google Colabの場合はこちら
@@ -30,9 +31,9 @@ data_test = pd.read_csv('C:/Users/ultra/Documents/GitHub/data/220417_kaggle_Prac
 data_test.head()
 
 
-# ## データ可視化
+# ## 1) データ可視化
 
-# In[52]:
+# In[4]:
 
 
 print('train data: ', np.shape(data_train))
@@ -42,14 +43,14 @@ print(data_train.info())
 
 
 # 
-# ### データをメタ化する
+# ### 1.1) データをメタ化する
 
-# In[84]:
+# In[5]:
 
 
 # 'meta'というDataFrameに、各columnについて以下カテゴリの分類結果を入れる
 # 'role': target, id, input
-# 'level': binary, nominal, interval, ordinal
+# 'level': binary, categorical, continuous, ordinal
 # 'keep': True except 'id'
 # 'dtype': int, float, str
 
@@ -67,14 +68,14 @@ for f in data_train.columns:
     
     # 'level'分類
     if 'bin' in f or f == 'target':
-        level = 'binary'
+        level = 'binary'   # バイナリ変数（0か1）のデータ
     elif 'cat' in f or f == 'id':
-        level = 'nominal'
+        level = 'categorical'   # カテゴリ変数のデータ
     elif data_train[f].dtype == float:
-        level = 'interval'
+        level = 'continuous'   # 浮動小数点のノーマルデータ
     #elif data_train[f].dtype == int:
     else:
-        level = 'ordinal'
+        level = 'ordinal'   # 整数のノーマルデータ
     
     # 'keep'分類
     keep = True
@@ -95,7 +96,7 @@ meta.set_index('varname', inplace=True)
 meta
 
 
-# In[90]:
+# In[6]:
 
 
 # 各levelに該当する説明変数の数をカウント
@@ -103,15 +104,15 @@ meta
 pd.DataFrame({'count': meta.groupby(['role','level'])['role'].count()}).reset_index()
 
 
-# ### データ全体の描写
+# ### 1.2) データ全体の描写
 
-# In[96]:
+# In[7]:
 
 
-# 'interval'変数を詳しく見る
+# 'continuous'変数を詳しく見る
 # ---------------------------
 
-v = meta[(meta['level'] == 'interval') & (meta['keep'] == True)].index
+v = meta[(meta['level'] == 'continuous') & (meta['keep'] == True)].index
 data_train[v].describe()
 
 # --------------------------
@@ -130,7 +131,7 @@ data_train[v].describe()
 # --------------------------
 
 
-# In[97]:
+# In[8]:
 
 
 # 'ordinal'変数を詳しく見る
@@ -153,7 +154,7 @@ data_train[v].describe()
 # --------------------------
 
 
-# In[100]:
+# In[9]:
 
 
 # 'binary'変数を詳しく見る
@@ -176,9 +177,9 @@ data_train[v].describe()
 # --------------------------
 
 
-# ### 欠損データの確認
+# ### 1.3) 欠損データの確認
 
-# In[109]:
+# In[10]:
 
 
 vars_missing = []
@@ -197,26 +198,70 @@ print('In total, there are {} missing variables'.format(len(vars_missing))
 
 # ----------------------------------
 # ・ps_car_03_cat、ps_car_05_catは特に欠損値の割合が大きい => 変数自体を取り除く
-# ・ps_reg_03は18％が欠損値 => 'interval'なのでmeanで置き換える
+# ・ps_reg_03は18％が欠損値 => 'continuous'なのでmeanで置き換える
+# ・その他欠損値も'continuous'はmean、'ordinal'はmodeで置き換える
 # ----------------------------------
 
 
-# ### nominal変数の特異点数の確認
+# ### 1.4) categorical変数の一意なデータ数の確認
 
-# In[113]:
+# In[11]:
 
 
-v = meta[(meta['level'] == 'nominal') & (meta['keep'] == True)].index
+v = meta[(meta['level'] == 'categorical') & (meta['keep'] == True)].index
 
 for f in v:
     dist_values = data_train[f].value_counts().shape[0]
     print('Varaiables {} has {} distinct values'.format(f, dist_values))
     
 # ----------------------------------
-# ・ps_car_11_catのみ極端に特異点数が多い
+# ・ps_car_11_catのみ極端に一意なデータ数が多い
 # ----------------------------------
 
 
-# ### target変数の不均衡対策
+# ## 2) データ加工
 
-# ### 欠損値処理
+# ### 2.1) target変数の不均衡対策
+
+# In[12]:
+
+
+# 1.2)で見たように'target'内で1の割合が極端に少ない
+# 対策として0をundersamplingして相対的に0と1の割合差を小さくする
+# --------------------------------------------------------------
+
+desired_apriori = 0.10   # 全体に対して1がこの割合になるよう0をundersampleする
+
+# 0と1のindexを取得
+idx_0 = data_train[data_train['target'] == 0].index
+idx_1 = data_train[data_train['target'] == 1].index
+
+nb_0 = len(idx_0)
+nb_1 = len(idx_1)
+
+# undersampling rateを計算
+undersampling_rate = ((1 - desired_apriori) * nb_1) / ((desired_apriori) * nb_0)
+undersampled_nb_0 = int(undersampling_rate * nb_0)
+
+print('Rate to undersample records with target=0: {}'.format(undersampling_rate))
+print('Number of records with target=0 after undrsampling: {}'.format(undersampled_nb_0))
+
+# 'target'=0のデータを必要な数だけランダムに選択する
+undersampled_idx = shuffle(idx_0, random_state=21, n_samples=undersampled_nb_0)
+
+# 'target'=0および1のリストを作成する
+idx_list = list(undersampled_idx) + list(idx_1)
+
+# undersampleしたデータを取り出す
+train = data_train.loc[idx_list].reset_index(drop=True)
+train
+
+
+# ### 2.2) 欠損値処理
+
+# In[ ]:
+
+
+# 欠損値の割合が大きい ps_car_03_cat、ps_car_05_cat を取り除く
+# -----------------------------------------------------------
+
